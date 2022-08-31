e44acbda6eb966e3b5b48fc2e32da7a8b5d43750
sebazelarayan12 <sebazelarayan12@gmail.com>
2022-08-31 20:26:08 -0300

    punto 1 objetos

---
+++ js/objetos.js
@@ -0,0 +1,11 @@
+let auto  = {
+    color: "rojo",
+    modelo: "toyota",
+    marca: "no se de autos",
+    encendido: true,
+}
+
+
+function encenderApagar(){
+    auto.encendido = !auto.encendido;
+}
