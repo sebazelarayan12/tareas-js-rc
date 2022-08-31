c648c26f69f56d46a18e638b11cd0f9b2436e474
sebazelarayan12 <sebazelarayan12@gmail.com>
2022-08-31 20:57:02 -0300

    hice punto 2 objetos

--- js/objetos.js
+++ js/objetos.js
@@ -1,11 +1,33 @@
-let auto  = {
-    color: "rojo",
-    modelo: "toyota",
-    marca: "no se de autos",
-    encendido: true,
-}
+// 1-
+
+// let auto  = {
+//     color: "rojo",
+//     modelo: "toyota",
+//     marca: "no se de autos",
+//     encendido: true,
+// }
 
 
-function encenderApagar(){
-    auto.encendido = !auto.encendido;
+// function encenderApagar(){
+//     auto.encendido = !auto.encendido;
+// }
+
+// 2-
+
+class cuenta {
+    constructor(titular, saldo){
+        this.titular =  titular;
+        this.saldo = 0;
+    }
+    ingresar (cantidad){
+        this.saldo = this.saldo + cantidad;
+    }
+    extraer (cantidad) {
+        this.saldo = this.saldo - cantidad;
+    }
+    informar(){
+        console.log(`usted tiene ${this.saldo}`);
+    }
 }
+
+let alex = new cuenta("Alex", 0)
