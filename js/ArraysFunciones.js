33a4a5cbf0154bf822c7306f2c3fa0480a321442
sebazelarayan12 <sebazelarayan12@gmail.com>
2022-08-31 11:38:40 -0300

    primero

---
+++ js/ArraysFunciones.js
@@ -0,0 +1,36 @@
+// 1-
+
+// let ciudades = [];
+// let ciudad = prompt("Ingrese una ciudad");
+
+// while (ciudad !== null && ciudad !== "0") {
+//   if (ciudad !== "") {
+//     ciudades.push(ciudad);
+//   }
+//   ciudad = prompt("Ingrese una ciudad");
+// }
+// console.log(ciudades);
+
+// console.log(`Cantidad de elementos en el arreglo: ${ciudades.length}`);
+
+// document.write(`1- ${ciudades[0]}<br>`);
+// document.write(`3- ${ciudades[2]}<br>`);
+// document.write(`ultima- ${ciudades[ciudades.length - 1]}<br>`);
+// ciudades.push("París");
+
+// document.write(`${ciudades[1]} ocupa la segunda posicion en el arreglo<br>`);
+
+// ciudades.splice(1, "Barcelona");
+
+
+// 2-
+
+// let a = prompt("ingrese longitud lado 1");
+// let b = prompt("ingrese longitud lado 2");
+
+// let perimetro = (a+b)*2;
+
+// alert(`El perimetro es ${perimetro}`); duda
+
+// 3-
+
---
+++ js/estructurasCondicionales.js
@@ -0,0 +1,72 @@
+// 1-
+
+// const diametro = prompt("Ingrese diametro (metros)");
+// const grosor = prompt("Ingrese grosor (metros)");
+
+// if (diametro > 1.4) {
+//   console.log("La rueda es para un vehiculo grande");
+// } else if (diametro <= 1.4 && diametro > 0.8) {
+//   console.log("La rueda es para un vehiculo mediano");
+// } else {
+//   console.log("La rueda es para un vehiculo pequeño");
+// }
+
+// b)
+
+// const diametro = prompt("Ingrese diametro (metros)");
+// const grosor = prompt("Ingrese grosor (metros)");
+
+// if (
+//   (diametro > 1.4 && grosor < 0.4) ||
+//   (diametro <= 1.4 && diametro > 0.8 && grosor < 0.25)
+// ) {
+//   console.log("El grosor para esta rueda es inferior al recomendado");
+// }
+
+// 2-
+
+// const topping = prompt("Ingrese el topping que quiere");
+
+// if (topping === "oreo"){
+//     console.log("El topping de oreo cuesta $1");
+// }
+// else if (topping === "kitkat"){
+//     console.log("El topping de kitkat cuesta 1.50");
+// }
+// else if (topping === "brownie"){
+//     console.log("El topping de brownie cuesta 0.75");
+// }
+// else{
+//     console.log("No disponemos de ese topping el precio del helado es 1.90");
+// }
+
+// 3-
+
+// let textos = [];
+
+// let texto = prompt('Ingrese un texto');
+
+// while (texto !== null && texto !== "0"){
+//     if (texto !== ""){
+//         textos.push(texto);
+//     }
+//     texto = prompt('Ingrese un texto');
+// }
+// console.log(textos.join("-"));
+
+// 4-
+
+// let suma = [];
+
+// let numero = prompt('Ingrese un numero');
+
+// while (numero !== null){
+//     if (numero > 0){
+//         suma.push(numero);
+//     }
+//     else {
+//         alert("Poner solo numeros");
+//     }
+//     numero = prompt('Ingrese un numero');
+// }
+// console.log(suma.reduce()); duda
